33e43d148da95c397bedd3e47a8d7b08c7b9eb89
chugad <69381456+chugad@users.noreply.github.com>
2020-09-22 22:23:29 +0700

    zadanie 2
    added PostManager.java

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,21 @@
+package manager;
+
+import domain.PostVk;
+
+public class PostManager {
+
+    public PostVk[] search() {
+        int ownerId; //идентификатор пользователя или сообщества.
+        String domain; //короткий адрес пользователя или сообщества.
+        String query; //поисковой запрос
+        boolean ownersOnly; //возвращать только записи от имени владельца стены.
+        int count; //количество записей, которые необходимо вернуть.
+
+        return null;
+    }
+
+    public void delete() {
+        int ownerId; //идентификатор пользователя или сообщества, на стене которого находится запись.
+        int postId; //идентификатор записи на стене
+    }
+}
