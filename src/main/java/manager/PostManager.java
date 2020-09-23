631031a23240325cf255a029ca0c79d8367ea7cd
chugad <69381456+chugad@users.noreply.github.com>
2020-09-23 09:14:13 +0700

    Update PostManager.java

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -4,18 +4,18 @@ import domain.PostVk;
 
 public class PostManager {
 
-    public PostVk[] search() {
-        int ownerId; //идентификатор пользователя или сообщества.
-        String domain; //короткий адрес пользователя или сообщества.
-        String query; //поисковой запрос
-        boolean ownersOnly; //возвращать только записи от имени владельца стены.
-        int count; //количество записей, которые необходимо вернуть.
+    public PostVk[] search(int ownerId, String domain, String query, boolean ownersOnly. int count) {
+        //int ownerId; //идентификатор пользователя или сообщества.
+        //String domain; //короткий адрес пользователя или сообщества.
+        //String query; //поисковой запрос
+        //boolean ownersOnly; //возвращать только записи от имени владельца стены.
+        //int count; //количество записей, которые необходимо вернуть.
 
         return null;
     }
 
-    public void delete() {
-        int ownerId; //идентификатор пользователя или сообщества, на стене которого находится запись.
-        int postId; //идентификатор записи на стене
+    public void delete(int ownerId, int postId) {
+        //int ownerId; //идентификатор пользователя или сообщества, на стене которого находится запись.
+        //int postId; //идентификатор записи на стене
     }
 }
